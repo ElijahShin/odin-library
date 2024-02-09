2a90812778989f1755ec7a1ad911f3901412f8b5
Elijah Shin <44964271+ElijahShin@users.noreply.github.com>
2024-02-09 10:47:26 -0800

    Add displayBookInfo function

--- js/index.js
+++ js/index.js
@@ -1,26 +1,54 @@
-const myLibrary = [];
+const myLibrary = [
+                    {
+                      title: `LOTR`,
+                      author: `J.R.R Tolkien`,
+                      pages: "295",
+                      isRead: false,
+                    }
+                  ];
 
-function Book(title, author, pageNum, isRead) {
+function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
-  this.pageNum = pageNum;
+  this.pages = pages;
   this.isRead = isRead;
 }
 
 Book.prototype.info = function() {
-  return `${this.title}, by ${this.author}, ${this.pageNum} pages`;
+  return `${this.title}, by ${this.author}, ${this.pages} pages`;
 }
 
 function addBookToLibrary() {
 
-  let title = prompt(`Book Title:`, "LOTR");
-  let author = prompt(`Book Author:`, "J.R.R Tolkien");
-  let pageNum = prompt(`Number of pages:`, 295);
+  let title = prompt(`Book Title:`, `LOTR`);
+  let author = prompt(`Book Author:`, `J.R.R Tolkien`);
+  let pages = prompt(`Number of pages:`, 295);
   let isRead = prompt(`isRead`, false);
 
-  const newBook = new Book(title, author, pageNum, isRead);
+  const newBook = new Book(title, author, pages, isRead);
   myLibrary.push(newBook);
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+function displayBookInfo() {
+  const main = document.querySelector('main');
+  const bookCard = document.createElement(`div`);
+  bookCard.className = `book-card`;
+  
+  myLibrary.forEach(ele => {
+
+    bookCard.innerHTML = 
+    `<p class="title">${ele.title}</p>
+    <p class="author">${ele.author}</p>
+    <p class="pages">${ele.pages}</p>
+    <button class="isread">${ele.isRead}</button>
+    <button class="delete">delete</button>`;
+    
+  })
+    
+  main.appendChild(bookCard);
+  
+}
+
+// addBookToLibrary();
+// console.log(myLibrary);
+displayBookInfo();
